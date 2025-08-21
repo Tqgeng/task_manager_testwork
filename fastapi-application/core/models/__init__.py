dfa8f2209b340f792f209e47b061c7ef51924d89
Tqgeng <pavel.kramarew@mail.ru>
2025-08-21 15:54:39 +0300

    add basic __init__.py and package structure

---
+++ fastapi-application/core/models/__init__.py
@@ -0,0 +1,13 @@
+__all__ = (
+    "db_helper",
+    "Base",
+    "User",
+    "AccessToken",
+    "Task",
+)
+
+from .db_helper import db_helper
+from .base import Base
+from .user import User
+from .access_token import AccessToken
+from .task import Task
